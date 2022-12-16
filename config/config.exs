3799dadd4cb119dd808001a25967820b5215e302
AlexV <asmodehn@gmail.com>
2022-12-16 17:02:03 +0100

    cleaned up git hooks config

--- config/config.exs
+++ config/config.exs
@@ -11,40 +11,19 @@ if Mix.env() == :dev do
     hooks: [
       pre_commit: [
         tasks: [
-          # stashing everything not staged, to test only what is in index
-          {:cmd, "git stash push -u --keep-index"},
-
-          # forceful formatting, careful: may create conflit with stash
+          # forceful formatting, careful: may create conflict with stash
           {:cmd, "mix format"},
+          # file are now formatted, but not added to commit just yet !
 
-          # CAREFUL: this will add all files that havent been stashed previously
-          {:cmd, "git add -u"},
-
-          # just once more to be sure
-          {:cmd, "mix format --check-formatted"},
-
-          # pop the stash to revert to current working tree
-          {:cmd, "git stash pop --index"}
-        ]
-      ],
-      pre_push: [
-        verbose: false,
-        tasks: [
-          # stashing everything, to test only what is in HEAD
-          {:cmd, "git stash push -u"},
-
-          # run the command that must succeed, before pushing and bothering CI
-          {:cmd, "mix dialyzer"},
-          # {:cmd, "mix credo --strict"},
-          {:cmd, "mix test --color"},
-
-          # pop the stash to revert to current working tree
-          {:cmd, "git stash pop"}
+          # run the command that must succeed, before committing and bothering CI
+          {:cmd, "mix dialyzer"}
+          # {:cmd, "mix test --color"}
         ]
       ],
       post_checkout: [
         tasks: [
-          {:cmd, "mix deps.get"}
+          {:cmd, "mix deps.get"},
+          {:cmd, "mix credo --strict"}
         ]
       ]
     ]
